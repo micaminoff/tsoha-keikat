eea3295ce0297fe2959f340596640f44ecd9c827
Michael Aminoff <micaminoff@gmail.com>
2020-03-19 11:11:00 +0200

    Updating information works

--- application/events/views.py
+++ application/events/views.py
@@ -1,7 +1,7 @@
 from application import app, db
 from flask import redirect, render_template, request, url_for
 from application.events.models import Event
-import datetime
+from datetime import datetime
 
 
 @app.route("/events", methods=["GET"])
@@ -20,7 +20,27 @@ def events_create():
     e = Event(name=data.get("name"),
               performer=data.get("performer"),
               venue=data.get("venue"),
-              date=datetime.datetime.strptime(data.get("date"), "%Y-%m-%d").date())
+              date=datetime.strptime(data.get("date"), "%Y-%m-%d").date())
     db.session().add(e)
     db.session().commit()
+    return redirect(url_for("events_index"))
+
+
+@app.route("/events/<event_id>/", methods=["GET"])
+def events_modify(event_id):
+    return render_template("events/modify.html",
+                           event=Event.query.get(event_id))
+
+
+@app.route("/events/<event_id>/", methods=["POST"])
+def events_update(event_id):
+    data = request.form
+    e = Event.query.get(event_id)
+    e.name = data.get("name")
+    e.performer = data.get("performer")
+    e.venue = data.get("venue")
+    e.date = datetime.strptime(data.get("date"), "%Y-%m-%d").date()
+
+    db.session().commit()
+
     return redirect(url_for("events_index"))
