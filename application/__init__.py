1770f0f6bb9a75e9b1189fe2bb5f4417f4eb62bb
Michael Aminoff <micaminoff@gmail.com>
2020-03-25 22:27:43 +0200

    Getting ready for deploy

--- application/__init__.py
+++ application/__init__.py
@@ -7,8 +7,13 @@ bcrypt = Bcrypt(app)
 
 # DB
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///events.db"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get('HEROKU'):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///events.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -23,8 +28,7 @@ from application.auth import views
 
 # Auth
 from application.auth.models import User
-from os import urandom
-app.config["SECRET_KEY"] = urandom(32)
+app.config["SECRET_KEY"] = os.urandom(32)
 
 from flask_login import LoginManager
 login_manager = LoginManager()
@@ -39,4 +43,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
