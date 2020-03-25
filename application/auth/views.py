585f01442c95df041f60ea268962e9103821c9ee
Michael Aminoff <micaminoff@gmail.com>
2020-03-25 17:14:30 +0200

    Logout and styling

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user
 
 from application import app
 from application.auth.models import User
@@ -17,8 +18,15 @@ def auth_login():
             email=form.email.data, password=form.password.data).first()
 
         if user:
+            login_user(user)
             print("User " + user.email + " signed in")
             return redirect(url_for("events_index"))
 
     return render_template("auth/loginform.html", form=form,
                            error="No such username or password")
+
+
+@app.route("/auth/logout")
+def auth_logout():
+    logout_user()
+    return redirect(url_for("events_index"))
