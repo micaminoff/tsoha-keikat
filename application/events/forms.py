616a38d95ac0e71f4af5e6d7c1874f2192ec2222
Michael Aminoff <micaminoff@gmail.com>
2020-04-02 20:28:25 +0300

    Fighting with multipleselect

--- application/events/forms.py
+++ application/events/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import DateField, SelectField, StringField, validators
+from wtforms import DateField, SelectField, SelectMultipleField, StringField, validators
 
 
 class EventForm(FlaskForm):
     name = StringField("Event name")
-    performer = SelectField(u'Performer', coerce=int)
+    #performer = SelectField(u'Performer', coerce=int)
+    performer = SelectMultipleField(u'Performer', coerce=int)
     venue = StringField('Venue', [validators.DataRequired()])
     date = DateField('Date', [validators.DataRequired()])
 
